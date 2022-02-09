06c8a616d3d498fb80e8253085504ff2ea72c165
jamullen08 <>
2022-02-09 05:15:56 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,6 @@
+temp = "this is a string"
+temp2 = "this is also a string"
+print(f"this is the value of temp: {temp}")
+print()
+print(f"and this is the value of temp+temp2: {temp}{temp2}")
+print()
---
+++ secondary.py
@@ -0,0 +1,2 @@
+print("this is the first line in the secondary file")
+print("and this is the second line")
